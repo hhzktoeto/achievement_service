ac039bcd42b97ef8493a450fa95a93eaeefc5a31
Rikaf <fakir207@mail.ru>
2024-02-24 18:01:32 +0500

    proccesinggg

--- src/main/java/faang/school/achievement/handler/ManagerAchievementHandler.java
+++ src/main/java/faang/school/achievement/handler/ManagerAchievementHandler.java
@@ -2,22 +2,29 @@ package faang.school.achievement.handler;
 
 import faang.school.achievement.cache.AchievementCache;
 import faang.school.achievement.dto.TeamEventDto;
+import faang.school.achievement.model.Achievement;
 import faang.school.achievement.service.AchievementService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
 public class ManagerAchievementHandler implements EventHandler<TeamEventDto> {
 
     private final AchievementCache achievementCache;
-    private AchievementService achievementService;
+    private final AchievementService achievementService;
 
     @Async
+    @Transactional
     @Override
     public void handle(Long userId) {
-            achievementCache.get()
+        Achievement achievement = achievementCache.get("MANAGER");
+
+        if (achievement != null && )
+
+
 
     }
 }
--- src/main/java/faang/school/achievement/service/AchievementService.java
+++ src/main/java/faang/school/achievement/service/AchievementService.java
@@ -1,5 +1,8 @@
 package faang.school.achievement.service;
 
+import faang.school.achievement.cache.AchievementCache;
+import faang.school.achievement.model.AchievementProgress;
+import faang.school.achievement.repository.AchievementRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,5 +11,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Slf4j
 public class AchievementService {
+
+    private final AchievementCache achievementCache;
+    private final AchievementRepository achievementRepository;
+
+    public boolean hasAchievment()
 
 }
