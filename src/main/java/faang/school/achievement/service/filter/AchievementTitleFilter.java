4388aad7e5880f466c2eca1c20719ac01b4cee40
DadyaVolodya <pl4stmass@yandex.ru>
2024-04-17 15:38:34 +0300

    по замечания кросс ревью

--- src/main/java/faang/school/achievement/service/filter/AchievementTitleFilter.java
+++ src/main/java/faang/school/achievement/service/filter/AchievementTitleFilter.java
@@ -5,6 +5,7 @@ import faang.school.achievement.model.Achievement;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+
 @Component
 public class AchievementTitleFilter implements AchievementFilter{
     @Override
