77e8d08482bfe63dd4a938371476097e2cb56a6f
DadyaVolodya <pl4stmass@yandex.ru>
2024-04-23 19:04:34 +0300

    ревью

--- src/main/java/faang/school/achievement/listener/EventListener.java
+++
@@ -1,8 +0,0 @@
-package faang.school.achievement.listener;
-
-import org.springframework.data.redis.connection.MessageListener;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface EventListener implements MessageListener {
-}
--- src/main/java/faang/school/achievement/listener/ProjectCreateEventListener.java
+++ src/main/java/faang/school/achievement/listener/ProjectCreateEventListener.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.serializer.SerializationException;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -16,7 +15,7 @@ import java.io.IOException;
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class ProjectCreateEventListener extends EventListener {
+public class ProjectCreateEventListener implements MessageListener {
     private ObjectMapper objectMapper;
     private BusinessmanAchievementHandler businessmanAchievementHandler;
 
